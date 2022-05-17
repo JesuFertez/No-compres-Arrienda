ed45415d3796284e288edd77fc0e4e85be353382
Jesufertez <maria.fernandez.cortez@gmail.com>
2022-05-17 16:32:58 -0400

    AI-Bumblebee | 2021.1.1 Patch 3 <jesu@MacBook-Air-de-Maria.local Overwrite remote https://github.com/JesuFertez/No-compres-arrienda.git to local

--- app/src/main/java/com/jesufertez/nocompresarrienda/model/Attributes.kt
+++
@@ -1,8 +0,0 @@
-package com.jesufertez.nocompresarrienda.model
-
-data class Attributes (
-    val engine : String,
-    val transmition: String,
-    val entertainment:String,
-    val security: String
-        )
--- app/src/main/java/com/jesufertez/nocompresarrienda/util/OnItemClick.kt
+++
@@ -1,5 +0,0 @@
-package com.jesufertez.nocompresarrienda.util
-
-interface OnItemClick {
-    fun onItemClick(position: Int,id : String)
-}
